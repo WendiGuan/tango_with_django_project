64e8376301de1f38a5fb932b97b16a8e5e1bc128
Wendi Guan <2721793G@student.gla.ac.uk>
2023-02-03 22:41:30 +0000

    the chapter 10 exercise completed

--- rango/views.py
+++ rango/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
-
+from datetime import datetime
 
 def index(request):
 
@@ -21,7 +21,12 @@ def index(request):
     context_dict['pages'] = page_list
    
     
-    return render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+
+    response = render(request, 'rango/index.html', context_dict)
+    request.session.set_test_cookie()  
+    return response
+
 
 
 def about(request):
@@ -29,8 +34,11 @@ def about(request):
     
     print(request.user)
 
-    return render(request, 'rango/about.html', {})
-
+    context_dict = {}
+    context_dict['visits'] = request.session['visits']
+    
+   
+    return render(request, 'rango/about.html', context_dict)
 
 
 
@@ -155,6 +163,32 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('rango:index'))
+
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+
+    if not val:
+        val = default_val
+
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
+
+
 
 
 
